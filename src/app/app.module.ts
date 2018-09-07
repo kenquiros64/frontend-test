9c04db884284453940f6897194629f51a71137c0
Kenneth Quirós <kenquiros64@gmail.com>
2018-09-07 08:16:02 -0600

    Adding ReactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
@@ -15,11 +15,12 @@ import { AuthService } from './auth.service';
     AppComponent,
     LoginComponent,
     AdminComponent,
-    HomeComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {
         path: 'login',
@@ -36,7 +37,8 @@ import { AuthService } from './auth.service';
       },
       {
         path: '',
-        component: HomeComponent
+        component: AdminComponent,
+        canActivate: [AuthGuard]
       }
     ])
   ],
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,29 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor(private Auth: AuthService) { }
+  constructor(private Auth: AuthService, private router: Router, private formBuilder:FormBuilder) { }
+
+  errorMessage = ""
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  isValidFormSubmitted = null;
+
+  userForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+  });
 
   ngOnInit() {
   }
 
   registerUser(event) {
     event.preventDefault()
-    const target = event.target
-    const email = target.querySelector('#email').value
     
-    var newUser = this.Auth.newUser(email)
-    if( newUser === true ){
-      console.log('New user')
-    }else{
-      console.log('User registered.')
+    this.isValidFormSubmitted = false;
+    if (this.userForm.invalid) {
+       return;
     }
-    target.querySelector('#email').value = ''
+    this.isValidFormSubmitted = true;
+    
+    let user = this.userForm.value;
+    console.log(user.email)
+    var newUser = this.Auth.newUser(user.email)
+    if( newUser === true ){
+      this.router.navigate(['admin'])
+      this.Auth.setLoggedIn(true)
+    }else{
+      this.errorMessage = 'User already registered.'
+    }
+    this.userForm.reset();
   }
+
+  get email() {
+    return this.userForm.get('email');
+ }   
 }
